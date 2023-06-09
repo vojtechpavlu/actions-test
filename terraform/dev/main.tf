53f63eb83877af2fa4fbf6dd826fdc19b3e8fd56
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2023-06-10 00:12:31 +0200

    Add full deployment

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -3,6 +3,22 @@ locals {
   security_group_ids = ["sg-0cf2209a5faf1c02c"]
   subnet_id          = "subnet-090eb586235ef3213"
   key_name           = "terraform-testing"
+  latest_version     = "99"
+  current_version    = var.override_version && var.parametric_version != "" ? var.parametric_version : local.latest_version
+  repository         = "https://github.com/vojtechpavlu/testing-project.git"
+  goto_folder        = "testing-project"
+}
+
+variable "override_version" {
+  type        = bool
+  description = "If the given version as a parameter should override the specified one"
+  default     = false
+}
+
+variable "parametric_version" {
+  type        = string
+  description = "Given variable as a version"
+  default     = ""
 }
 
 module "instance" {
@@ -13,13 +29,27 @@ module "instance" {
   security_group_ids = local.security_group_ids
   subnet_id          = local.subnet_id
   key_name           = local.key_name
+
+  user_data = <<EOF
+#! /bin/sh
+sudo yum update -y
+sudo yum install docker -y
+sudo yum install git -y
+
+sudo service docker start
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+
+git clone ${local.repository}
+cd ${local.goto_folder}
+
+sudo docker build . -t app
+sudo docker run -e version='${local.current_version}' -p 80:8080 app
+
+EOF
 }
 
 resource "local_file" "deployed_instance_metadata" {
   filename = "${path.module}/deployment_info.txt"
-  content  = <<EOF
-Instance ID: ${module.instance.instance_id}
-Public DNS: ${module.instance.public_dns}
-Public IP: ${module.instance.public_ip}
-EOF
+  content  = jsondecode(module.instance)
 }
