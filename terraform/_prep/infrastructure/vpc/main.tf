bf1ad0c9c39e835103d1adddcdd702c77397989b
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2023-06-13 12:41:56 +0200

    Add DNS assignment to VPC

--- terraform/_prep/infrastructure/vpc/main.tf
+++ terraform/_prep/infrastructure/vpc/main.tf
@@ -1,5 +1,6 @@
 resource "aws_vpc" "created_vpc" {
-  cidr_block = var.ip_cidr_range
+  cidr_block           = var.ip_cidr_range
+  enable_dns_hostnames = true   # Enables DNS hostname assignment
 
   tags = {
     Name = var.vpc_name
