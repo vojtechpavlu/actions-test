1bd110825644ec011ea58bc254d7711692c9ae5a
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2023-06-16 12:50:27 +0200

    Add AWS credentials

--- terraform/_prep/infrastructure/security_groups.tf
+++ terraform/_prep/infrastructure/security_groups.tf
@@ -1,8 +1,23 @@
-module "security_groups" {
-  source = "./security_group"
+module "dev_sg" {
+  source  = "./security_group"
+  sg_name = "dev_security_group"
+  vpc_id  = module.vpc.vpc_id
+}
 
-  for_each = toset(["dev", "test", "prod"])
+module "test_sg" {
+  source  = "./security_group"
+  sg_name = "test_security_group"
+  vpc_id  = module.vpc.vpc_id
+}
 
-  sg_name = "${each.key}_security_group"
-  vpc_id = module.vpc.vpc_id
+module "prod_sg" {
+  source  = "./security_group"
+  sg_name = "prod_security_group"
+  vpc_id  = module.vpc.vpc_id
+}
+
+module "manager_sg" {
+  source  = "./security_group"
+  sg_name = "manager_security_group"
+  vpc_id  = module.vpc.vpc_id
 }
